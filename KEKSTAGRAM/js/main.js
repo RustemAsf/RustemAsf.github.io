4fd2c67ff70481313b10f56f610b04135a2fd331
RustemAsf <87094009+RustemAsf@users.noreply.github.com>
2022-04-22 17:24:22 +0500

    Delete KEKSTAGRAM directory

--- KEKSTAGRAM/js/main.js
+++
@@ -1,9 +0,0 @@
-import {onImageOverlayClose} from './form.js';
-import {setUserFormSubmit} from './validation.js';
-import './form.js';
-import './scale.js';
-import './slider.js';
-import './filters.js';
-import './user-photo.js';
-
-setUserFormSubmit(onImageOverlayClose);
